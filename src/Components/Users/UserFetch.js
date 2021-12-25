99c80e6adf322796d4b75a67020065f1f6ac846f
kamruz-zaman8991 <86624274+kamruz-zzaman@users.noreply.github.com>
2021-12-26 00:02:54 +0600

    design success

--- src/Components/Users/UserFetch.js
+++ src/Components/Users/UserFetch.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Users from './Users';
+import './user.css'
+import avater from '../../Images/avater.png'
 
 const UserFetch = () => {
     const [users, setUsers] = useState([]);
@@ -10,15 +12,37 @@ const UserFetch = () => {
     }, [])
     return (
         <>
-            <div>
-                {
-                    users.map(user => (
-                        <Users
-                            key={user.id}
-                            users={user}
-                        ></Users>
-                    ))
-                }
+            <div className='fds d-flex'>
+                <div className=' m-5 divWidth '>
+                    <h4 className='usersList'>USERS LIST</h4>
+                    {/* <img className='border' src="../../Images/avater.png" alt="" /> */}
+                    {
+                        users.map(user => (
+                            <Users
+                                key={user.id}
+                                users={user}
+                            ></Users>
+                        ))
+                    }
+                </div>
+                <div className='divWidth m-5 '>
+                    <h4 className='usersList'>USER DETAILS</h4>
+                    <img width="15%" className='mt-3' src={avater} alt="" /><br />
+                    <h3>name</h3>
+                    <textarea name="" id="" cols="30" rows="3"></textarea><br />
+                    <div className='mt-4 '>
+                        <span className='inputTitle'>Full name: </span><br />
+                        <input className='w-50' type="text" name="" id="" />
+                    </div>
+                    <div className='mt-2'>
+                        <span className='inputTitle2'>Job Title: </span><br />
+                        <input className='w-50' type="text" name="" id="" />
+                    </div>
+                    <div className='mt-2'>
+                        <span className='inputTitle3'>Email: </span><br />
+                        <input className='w-50' type="text" name="" id="" />
+                    </div>
+                </div>
             </div>
         </>
     );
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = ({ users }) => {
     const { avatar, profile } = users;
     return (
         <div>
-            <p>{profile.firstName} {profile.lastName}</p>
+            <Link to='' className='link'><p className='bg-danger'><img src="" alt="" />{profile.firstName} {profile.lastName}</p></Link>
         </div>
     );
 };
