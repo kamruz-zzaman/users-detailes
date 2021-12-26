8b650935d9d2df140747fa36dad539fc83c7ef98
kamruz-zaman8991 <86624274+kamruz-zzaman@users.noreply.github.com>
2021-12-26 14:43:33 +0600

    delet useless file

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const Users = ({ users, handleUser }) => {
-    const { id, avatar, profile } = users;
+    const { id, profile } = users;
 
     return (
         <div>
+
             <button onClick={() => handleUser(id)} className='userBg py-2 mt-3 w-100'>{profile.firstName} {profile.lastName}</button>
 
         </div>
